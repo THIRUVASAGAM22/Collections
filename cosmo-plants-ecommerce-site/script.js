18682733ef124689086f236cb8cb8e02e7466746
THIRUVASAGAM22 <124457012+THIRUVASAGAM22@users.noreply.github.com>
2025-11-07 10:02:33 +0530

    Add files via upload

---
+++ cosmo-plants-ecommerce-site/script.js
@@ -0,0 +1,6 @@
+// Simple cart popup simulation
+document.querySelectorAll(".buy-btn").forEach(btn => {
+  btn.addEventListener("click", () => {
+    alert("Added to cart! (Demo only)");
+  });
+});
